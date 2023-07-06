49578ae375d9fa4755e46331987d4668f3584c55
CristinaResina <resina.cristina96@gmail.com>
2023-07-06 10:05:58 -0700

    update

--- js/dom-play.js
+++ js/dom-play.js
@@ -13,3 +13,42 @@
 */
 
 //document.querySelector(".hamlet").style.backgroundColor="yellow";
+
+function highlight(el){
+
+    if(el.style.backgroundColor=='white'){//change to blue
+        el.style.backgroundColor='blue';
+    }else{//change to withe 
+        el.style.backgroundColor='white';
+    }
+
+}
+
+function myAlert(){
+    alert("I'm clicked!");
+}
+
+function highlightActor(actor){
+    //alert("The current actor is : " + actor);
+    for(const mySpan of spans){
+        if(actor==mySpan.dataset.actor){//current actor
+            mySpan.style.backgroundColor='blue';    
+        }else{//other actor
+            mySpan.style.backgroundColor='white';
+        }
+    }
+}
+
+const spans=document.querySelectorAll("#play span");
+
+console.log(spans);
+
+for(const mySpan of spans){
+    //mySpan.addEventListener("click",myAlert);
+    mySpan.addEventListener("click",function(ev){
+        highlightActor(mySpan.dataset.actor);
+    });
+
+}
+
+
